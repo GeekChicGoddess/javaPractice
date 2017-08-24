5acfe5e8ba435cc10c92e2449ab44db3dacd8cc2
melodytempleton <mtempletoncantu@gmail.com>
2017-08-23 20:27:05 -0500

    more recursion challenges

--- src/recursion1.java
+++ src/recursion1.java
@@ -61,6 +61,72 @@ public class recursion1 {
 
         }
     }
+    public String changePi(String str) {
+        int length = str.length();
+        if (length == 0) return "";
+        if (length == 1) return str;
+        if(length == 2){
+            if (str.equals("pi")) return "3.14";
+            return str;
+        }
+        char [] strArray = str.toCharArray();
+
+        char [] shortArray = Arrays.copyOfRange(strArray, 0, 2);
+        String shortStr = String.valueOf(shortArray);
+        System.out.println("1 shortStr "  + shortStr);
+
+        if (shortStr.equals("pi")){
+            shortStr = "3.14";
+            char [] newstrArray = Arrays.copyOfRange(strArray, 2, length);
+            str = String.valueOf(newstrArray);
+            System.out.println("2 str " + str);
+        }
+        else{
+            char [] newstrArray = Arrays.copyOfRange(strArray, 1, length);
+            str = String.valueOf(newstrArray);
+            shortArray = Arrays.copyOfRange(strArray, 0, 1);
+            shortStr = String.valueOf(shortArray);
+            System.out.println("3 boht " + shortStr + " " +str);
+
+        }
+
+        return shortStr + changePi(str);
+    }
+
+
+    public String parenBit(String str) {
+
+        char[] charArray = str.toCharArray();
+
+        String paranStr = "";
+        System.out.println(str);
+        System.out.println(charArray[charArray.length-1]);
+
+        if(charArray[charArray.length-1] == '('){
+            if(charArray[0] == ')'){
+                return ")";
+            }
+            else {
+                paranStr = String.valueOf(charArray[0]);
+                charArray = Arrays.copyOfRange(charArray, 1, charArray.length);
+            }
+        }
+
+        else if (charArray[0] == '('){
+            str = str + "(";
+            charArray = str.toCharArray();
+            charArray =  Arrays.copyOfRange(charArray, 1, charArray.length);
+            paranStr= "(";
+        }
+        else {
+            charArray =  Arrays.copyOfRange(charArray, 1, charArray.length);
+        }
+        str = String.valueOf(charArray);
+
+
+        return  paranStr + parenBit(str) ;
+    }
+
 
 
     public static void main(String[] args) {
@@ -69,6 +135,8 @@ public class recursion1 {
 
 //        System.out.println(rec.countX("ixhxiix"));
 
-        System.out.println(rec.countHi("ihihihi"));
+//        System.out.println(rec.countHi("ihihihi"));
+//        System.out.println(rec.changePi("ipipipi"));
+        System.out.println(rec.parenBit("ipi(sdsd)i"));
     }
 }
